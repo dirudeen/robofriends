9e6e58697c341c21a66cb05761cf959d3da63c3a
Hadiru Colley <dirudeen22@gmail.com>
2023-07-29 01:54:03 +0300

    add componentDidMount and json placeholder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
 import CardLists from "./components/CardLists";
-import { robots } from "./robots";
 import Searchbox from "./components/Searchbox";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ``,
     };
+  }
+
+  componentDidMount() {
+    try {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((respones) => {
+          return respones.json();
+        })
+        .then((Users) => {
+          if (!Users.length) {
+            return <h2>Loading</h2>;
+          } else {
+            this.setState({ robots: Users });
+          }
+        });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   onsearchChange = (e) => {
